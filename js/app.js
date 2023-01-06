1d6fa8b788e878420dca38f3414205069497c518
Manuchehr-1998 <120194005+Manuchehr-1998@users.noreply.github.com>
2023-01-06 15:00:09 +0500

    sdsad

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let date = new Date();
+console.log(
+  date.getDate(),
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+  date.getMilliseconds(1)
+);
+// 6-date 14-hours 44-minutes 42-seconds 760-milliseconds
